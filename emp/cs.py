a217529e5fda4b71c305e46835d465db0b39242a
Train0303 <rjsdnxogh55@gmail.com>
2023-11-27 17:00:05 +0900

    Fix: cs 로그인 과정 오류 수정

--- emp/cs.py
+++ emp/cs.py
@@ -2,8 +2,10 @@ from employee import Employee
 ## Custom Service 비지니스 로직
 
 class CustomService(Employee):
-    def __init__(self, id):
-        super('role_cs')
+    role = 'role_cs'
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
         with self.conn.cursor() as cur:
             # 트랜잭션에 영향을 주지않는 세션 레벨의 명령어 -> commit()을 해줄 필요 없다.
