a507d2917b0789e93d5c2cf99213b650c29d9519
Train0303 <rjsdnxogh55@gmail.com>
2023-11-29 22:50:05 +0900

    Feat: 더미데이터 생성 코드 이전

---
+++ emp/emp_init.sql
@@ -0,0 +1,20 @@
+insert into member_tb (id, name, pw, role) values 
+('buy4', 'buy4', 'qwer1234', 'role_buyer'),
+('buy5', 'buy5', 'qwer1234', 'role_buyer'),
+('buy6', 'buy6', 'qwer1234', 'role_buyer');
+
+INSERT INTO product_tb(name, kind, description) VALUES
+('얼음골 사과 1kg 팝니다', '사과', '얼음골 꿀사과 1kg 만원입니다.'),
+('나주 배 1kg 팝니다', '배', '유명한 나주 배 1kg 만원에 팝니다!'),
+('각 수확한 수박팝니다', '수박', '싱싱한 수박 팝니다!!'),
+('올해 수확한 햇쌀 팝니다', '쌀', '올해 수확한 햇쌀 20kg당 2만원에 팝니다.');
+
+INSERT INTO auction_tb(id, sel_id, buy_id, emp_id, product_id, price, verified, count, start_time, end_time) VALUES
+(1, 'seller', NULL, NULL, 1, 10000, 'Y', 5, '2023-11-25 13:00:00', '2023-12-30 15:00:00'),
+(2, 'seller', NULL, NULL, 2, 10000, 'Y', 2, '2023-11-29 15:30:00', '2023-12-30 16:30:00'),
+(3, 'seller', NULL, NULL, 3, 20000, 'N', 4, '2023-11-29 12:13:14', '2023-12-25 15:00:00');
+
+INSERT INTO auction_record_tb(id, buy_id, auc_id, price, order_time) VALUES
+(1, 'buy4', 1, 15000, NULL),
+(2, 'buy5', 1, 20000, NULL),
+(3, 'buy6', 2, 30000, NULL);
--- init.sql
+++ init.sql
@@ -304,14 +304,4 @@ BEFORE INSERT ON auction_record_tb
 FOR EACH ROW
 EXECUTE FUNCTION check_auction_record_validation();
 
-INSERT INTO product_tb(name, kind, description) VALUES
-('얼음골 사과 1kg 팝니다', '사과', '얼음골 꿀사과 1kg 만원입니다.'),
-('나주 배 1kg 팝니다', '배', '유명한 나주 배 1kg 만원에 팝니다!'),
-('각 수확한 수박팝니다', '수박', '싱싱한 수박 팝니다!!'),
-('올해 수확한 햇쌀 팝니다', '쌀', '올해 수확한 햇쌀 20kg당 2만원에 팝니다.');
-
-INSERT INTO auction_tb(sel_id, buy_id, emp_id, product_id, price, verified, count, start_time, end_time) VALUES
-('seller', NULL, NULL, 1, 10000, 'N', 5, '2023-11-28 13:00:00', '2023-11-28 15:00:00'),
-('seller', NULL, NULL, 2, 10000, 'N', 2, '2023-11-30 15:30:00', '2023-11-30 16:30:00'),
-('seller', NULL, NULL, 3, 20000, 'N', 4, '2024-06-23 12:13:14', '2024-06-24 15:00:00');
 
